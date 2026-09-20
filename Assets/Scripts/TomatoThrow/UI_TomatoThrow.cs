970ae809ce28d550dbf86ced0fb55bb137dfaa24
PinsonX <219718030+beepboop860@users.noreply.github.com>
2026-09-20 15:03:27 -0400

    tomato in start menu and other levels

--- Assets/Scripts/TomatoThrow/UI_TomatoThrow.cs
+++ Assets/Scripts/TomatoThrow/UI_TomatoThrow.cs
@@ -131,7 +131,7 @@ public class UI_ThrowableTomato : MonoBehaviour
         mousePosition.y = Mathf.Clamp(mousePosition.y, initialPosition.y, targetHeightY * 0.75f);
         //mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         newPosition = mousePosition;
-        transform.position = newPosition;
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, 0.5f * Vector3.Distance(transform.position, newPosition));
     }
 
     public bool HasThrowVelocity()
